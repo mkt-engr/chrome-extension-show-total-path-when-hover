a34bbfad655c23f1300eb640f06027706e13a335
Makito Mori <66419373+mkt-engr@users.noreply.github.com>
2025-02-19 21:56:12 +0900

    .

--- pages/content/src/findFileNavigation.ts
+++ pages/content/src/findFileNavigation.ts
@@ -1,11 +1,33 @@
 export const findFileNavigation = () => {
   console.log('findFileNavigation() called');
-  const targetElement = document.querySelector<HTMLElement>('nav[aria-label="File Tree Navigation"]');
+  const targetElements = document.querySelectorAll<HTMLElement>('span.Truncate');
 
-  if (targetElement) {
-    console.log('File navigation found');
-    targetElement.style.backgroundColor = 'red';
-  } else {
+  if (targetElements.length === 0) {
     alert('File navigation not found');
   }
+
+  console.log('File navigation found');
+  targetElements.forEach(targetElement => {
+    targetElement.style.backgroundColor = 'red';
+
+    // targetElementの直下のaタグを取得
+    const anchorElement = targetElement.querySelector('a');
+    const anchorText = anchorElement?.textContent ?? 'No anchor text';
+
+    const match = anchorText.match(/\/src\/.*/);
+
+    const a = match ? match[0] : anchorText;
+
+    // 新しいボタン要素を作成
+    const button = document.createElement('button');
+    button.textContent = 'コピー';
+    button.style.marginLeft = '10px'; // ボタンとtargetElementの間にスペースを追加
+    button.addEventListener('click', () => {
+      navigator.clipboard.writeText(a);
+      alert(`copy ${a}`);
+    });
+
+    // ボタンをtargetElementの隣に配置
+    targetElement.insertAdjacentElement('afterend', button);
+  });
 };
