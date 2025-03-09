b03d8051bb3ed752ce7633267eb646ba9dd719eb
Makito Mori <66419373+mkt-engr@users.noreply.github.com>
2025-03-09 14:16:40 +0900

    .

--- chrome-extension/src/background/index.ts
+++ chrome-extension/src/background/index.ts
@@ -7,3 +7,7 @@ exampleThemeStorage.get().then(theme => {
 
 console.log('Background loaded');
 console.log("Edit 'chrome-extension/src/background/index.ts' and save to reload.");
+
+// chrome.webNavigation.onCommitted.addListener(details => {
+//   console.log('chrome.webNavigation.onCommitted.addListener', details);
+// });
