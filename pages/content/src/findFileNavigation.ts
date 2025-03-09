c0fdd0bb721693e42ac0bdc2b7b41aa7cdd4cfd1
Makito Mori <66419373+mkt-engr@users.noreply.github.com>
2025-03-09 16:49:20 +0900

    不要なコメント削除

--- pages/content/src/findFileNavigation.ts
+++ pages/content/src/findFileNavigation.ts
@@ -1,42 +1,34 @@
 const BUTTON_ID_PREFIX = 'chrome-extension-copyButton';
 
 export const findFileNavigation = () => {
-  console.log('findFileNavigation() called');
-
-  const targetElements = document.querySelectorAll<HTMLElement>('span.Truncate');
-
-  if (targetElements.length === 0) {
-    // alert('File navigation not found');
-  }
-
-  console.log('File navigation found');
-
   const existingButton = document.getElementById(`${BUTTON_ID_PREFIX}-0`);
   if (existingButton) {
-    console.log('Buttons already exist, skipping...');
     return;
   }
 
+  const a = document.querySelectorAll('clipboard-copy');
+  console.log(a);
+  const targetElements = document.querySelectorAll<HTMLElement>('span.Truncate');
   targetElements.forEach((targetElement, index) => {
-    const buttonId = `${BUTTON_ID_PREFIX}-${index}`;
-    targetElement.style.backgroundColor = 'red';
-
     // targetElementの直下のaタグを取得
     const anchorElement = targetElement.querySelector('a');
+    // const anchorElement = targetElement.querySelector('clipboard-copy');
+    // console.log(anchorElement?.value);
     const anchorText = anchorElement?.textContent ?? 'No anchor text';
+    // const anchorText = anchorElement?.value ?? 'No anchor text';
 
-    const match = anchorText.match(/\/src\/.*/);
-
-    const a = match ? match[0] : anchorText;
+    // src以降のテキストを取得
+    const match = anchorText.match(/src\/.*/);
+    const textToCopy = match ? match[0] : anchorText;
 
     // 新しいボタン要素を作成
     const button = document.createElement('button');
-    button.id = buttonId;
+    button.id = `${BUTTON_ID_PREFIX}-${index}`;
     button.textContent = 'コピー';
     button.style.marginLeft = '10px'; // ボタンとtargetElementの間にスペースを追加
     button.addEventListener('click', () => {
-      navigator.clipboard.writeText(a);
-      alert(`copy ${a}`);
+      navigator.clipboard.writeText(textToCopy);
+      alert(`copy ${textToCopy}`);
     });
 
     // ボタンをtargetElementの隣に配置
--- pages/content/src/sampleFunction.ts
+++ pages/content/src/sampleFunction.ts
@@ -1,30 +1,11 @@
 import { findFileNavigation } from './findFileNavigation';
 
-let isCalled = false;
-
 export function sampleFunction() {
-  console.log('content script - sampleFunction()が呼ばれた', { isCalled });
-
   // URLの変更をchrome.webNavigation.onHistoryStateUpdatedで検知
-
   chrome.runtime.onMessage.addListener(message => {
-    console.log('chrome.runtime.onMessage.addListener', message.url);
-    console.log({ isCalled });
     if (message.type === 'URL_CHANGED') {
       console.log('URLが変更されました！', message.url);
       findFileNavigation();
-      isCalled = true;
     }
   });
-
-  const observer = new MutationObserver(function () {
-    const elm = document.getElementById('piyo');
-    if (elm) {
-      // 対象の要素が読み込まれたので、ここで好きなことを行う
-      // これ以上監視を続けない場合は、observer.disconnect()する
-    }
-  });
-
-  const observerTarget = document.querySelector('body')!;
-  observer.observe(observerTarget, { childList: true, subtree: true });
 }
